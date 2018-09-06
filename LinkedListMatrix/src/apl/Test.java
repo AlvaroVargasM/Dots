ec6aaf92c8155e08cbb55d48bbaf4ec305f5164b
ErickCR12 <erick.cr12@outlook.com>
2018-09-06 17:44:06 -0600

    Modificacion test

--- LinkedListMatrix/src/apl/Test.java
+++ LinkedListMatrix/src/apl/Test.java
@@ -8,13 +8,14 @@ public class Test {
     public static void main(String[] args){
         
         Matrix matrix = new Matrix();
-        for (int i = 0; i < 3; i++){
+        int pos = 0;
+        for (int i = 0; i < 5; i++){
             LinkedList list = new LinkedList(i);
-            for(int j = 0; j < 3; j++){
-                list.insertNode(j);
+            for(int j = 0; j < 5; j++){
+                list.insertNode(pos++);
             }
             matrix.insertList(list);
         }
-        System.out.println(matrix.toString());
+        System.out.println(matrix.toString());        
     }
 }
