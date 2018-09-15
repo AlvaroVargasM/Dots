db104a13b09c5c42b06641371b45959a3e0d564e
ErickCR12 <erick.cr12@outlook.com>
2018-09-14 19:39:34 -0600

    Cambio de nombre a atributos

--- LinkedListMatrix/src/board/LinkedListNode.java
+++ LinkedListMatrix/src/board/LinkedListNode.java
@@ -1,56 +1,109 @@
 package board;
 
+/**
+ * Node for LinkedList class. Its main purpose is to store the data needed for the
+ * LinkedList and point to the next Node in the list.
+ * @author Erick Barrantes
+ * @param <GenericType>
+ */
 public class LinkedListNode<GenericType> {
     private int position;
     private GenericType data;
-    private LinkedListNode nextDot;
+    private LinkedListNode nextNode;
     private LinkedList connectionsList;
     private boolean visited;
     
+    /**
+     * LinkedListNode constructor. Receives an integer that indicates its position
+     * in the list. Also, it can store a data with any kind of type (GenericType).
+     * Assigns all the values to the attributes.
+     * @param position
+     * @param data
+     */
     public LinkedListNode(int position, GenericType data){
         this.position = position;
-        this.nextDot = null;
+        this.nextNode = null;
         this.connectionsList = new LinkedList(0);
         this.visited = false;
         this.data = data;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPosition() {
         return position;
     }
 
+    /**
+     *
+     * @param position
+     */
     public void setPosition(int position) {
         this.position = position;
     }
 
-    public LinkedListNode getNextDot() {
-        return nextDot;
+    /**
+     *
+     * @return
+     */
+    public LinkedListNode getNextNode() {
+        return nextNode;
     }
 
-    public void setNextDot(LinkedListNode nextDot) {
-        this.nextDot = nextDot;
+    /**
+     *
+     * @param nextNode
+     */
+    public void setNextNode(LinkedListNode nextNode) {
+        this.nextNode = nextNode;
     }
 
+    /**
+     *
+     * @return
+     */
     public LinkedList getConnectionsList() {
         return connectionsList;
     }
 
+    /**
+     *
+     * @param connectionsList
+     */
     public void setConnectionsList(LinkedList connectionsList) {
         this.connectionsList = connectionsList;
     }
 
+    /**
+     *
+     * @return
+     */
     public GenericType getData() {
         return data;
     }
 
+    /**
+     *
+     * @param data
+     */
     public void setData(GenericType data) {
         this.data = data;
     }
     
+    /**
+     *
+     * @return
+     */
     public boolean isVisited() {
         return visited;
     }
 
+    /**
+     *
+     * @param visited
+     */
     public void setVisited(boolean visited) {
         this.visited = visited;
     }
--- LinkedListMatrix/src/board/QueueNode.java
+++ LinkedListMatrix/src/board/QueueNode.java
@@ -1,25 +1,49 @@
 package board;
 
+/**
+ *
+ * @author Erick Barrantes
+ */
 public class QueueNode {
     private QueueNode nextNode;
     private Player player;
     
+    /**
+     *
+     * @param player
+     */
     public QueueNode(Player player){
         this.player = player;
     }
 
+    /**
+     *
+     * @return
+     */
     public QueueNode getNextNode() {
         return nextNode;
     }
 
+    /**
+     *
+     * @param nextNode
+     */
     public void setNextNode(QueueNode nextNode) {
         this.nextNode = nextNode;
     }
 
+    /**
+     *
+     * @return
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     *
+     * @param player
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
