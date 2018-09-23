6a01ee2632e9b30c06b35819991f9c93ec877d41
ErickCR12 <erick.cr12@outlook.com>
2018-09-23 14:43:33 -0600

    Ganar partida es validado

--- View/src/controller/ClientController.java
+++ View/src/controller/ClientController.java
@@ -188,7 +188,7 @@ public class ClientController implements Runnable{
     public static void clientSend(Object object, Object classReference){
         try {
             //"192.168.100.14"
-            Socket clientSocket = new  Socket("192.168.100.9", 9090);
+            Socket clientSocket = new  Socket("192.168.100.14", 9090);
             
             String sendObject = JSONUtil.convertJavaToJson(object);
             String sendClassReference = JSONUtil.convertJavaToJson(classReference);
