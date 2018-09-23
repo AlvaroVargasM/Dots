e139687567edf01c73d69c99528c9428d2717bd5
Lumanter <36551954+Lumanter@users.noreply.github.com>
2018-09-23 14:24:34 -0600

    .

--- View/src/dataPackages/DotConnectionPack.java
+++ View/src/dataPackages/DotConnectionPack.java
@@ -1,30 +1,56 @@
+
 package dataPackages;
 
 import java.io.Serializable;
 
+/**
+ *
+ * @author luism
+ */
 public class DotConnectionPack implements Serializable{
     
     private int initialDot;
     private int finalDot;
     private int playerNumber;
 
+    /**
+     *
+     * @param initialDot
+     * @param finalDot
+     * @param playerNumber
+     */
     public DotConnectionPack(int initialDot, int finalDot, int playerNumber) {
         this.initialDot = initialDot;
         this.finalDot = finalDot;
         this.playerNumber = playerNumber;
     }
     
+    /**
+     *
+     */
     public DotConnectionPack(){
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPlayerNumber() {
         return playerNumber;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getInitialDot() {
         return initialDot;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getFinalDot() {
         return finalDot;
     }
--- View/src/dataPackages/ToFigurePack.java
+++ View/src/dataPackages/ToFigurePack.java
@@ -1,22 +1,51 @@
+
 package dataPackages;
 
 import java.io.Serializable;
 
+/**
+ *
+ * @author luism
+ */
 public class ToFigurePack implements Serializable{
+
+    /**
+     *
+     */
     public String figure;
+
+    /**
+     *
+     */
     public int playerNumber;
 
+    /**
+     *
+     * @param figure
+     * @param playerNumber
+     */
     public ToFigurePack(String figure, int playerNumber) {
         this.figure = figure;
         this.playerNumber = playerNumber;
     }
     
+    /**
+     *
+     */
     public ToFigurePack(){}
 
+    /**
+     *
+     * @return
+     */
     public String getFigure() {
         return figure;
     }
     
+    /**
+     *
+     * @return
+     */
     public int getPlayerNumber(){
         return playerNumber;
     }
